7e43109e251e18dae58b5448e7e2b7aaf1c96bbe
thanhlong0728 <thanhlong0728@gmail.com>
2024-04-21 10:01:05 +0700

    update list notice

--- app/screens/Notification/index.js
+++ app/screens/Notification/index.js
@@ -13,7 +13,10 @@ import Ionicons from 'react-native-vector-icons/Ionicons';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import {useNavigation} from '@react-navigation/native';
 
-const notifications = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+const notifications = [
+  1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22,
+  23, 24, 25,
+];
 
 const NotificationScreen = () => {
   const navigation = useNavigation();
